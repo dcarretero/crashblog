b9d2175a2297df1dfd8648b28823bb4633b2f401
Daniel Carretero <daniel.carretero@gmail.com>
2024-12-28 20:02:47 +0100

    Version que permite ver el detalle de un post y añadir comentarios

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -24,4 +24,18 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
     
--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Post
+from .forms import CommentForm
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/detail.html', {'post': post})
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('blog:post_detail', slug=slug)
+    else:
+        form = CommentForm()
+    
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
